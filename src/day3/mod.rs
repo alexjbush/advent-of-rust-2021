fdb762207a7f8b6a8f043f4a263904d16b245da4
Alex Bush <bushnoh@gmail.com>
2021-12-08 23:29:16 +1300

    Day 3 part 2

--- src/day3/mod.rs
+++ src/day3/mod.rs
@@ -1,5 +1,3 @@
-use std::str::Lines;
-
 use crate::Day;
 
 pub struct Day3 {}
@@ -23,7 +21,50 @@ fn task1() -> String {
 
 fn task2() -> String {
     let parsed_input = get_input(INPUT);
-    "N/A".to_string()
+    let max = get_rating(&parsed_input, |a, b| a * 2 >= b);
+    let min = get_rating(&parsed_input, |a, b| a * 2 < b);
+    (max * min).to_string()
+}
+
+fn get_rating<F>(input: &Vec<&str>, comp: F) -> u32
+where
+    F: Fn(u32, u32) -> bool,
+{
+    let mut counts: Vec<Vec<u32>> = input
+        //.clone()
+        .into_iter()
+        .map(|l| {
+            //Line
+            l.chars()
+                .map(|c| c.to_string().parse::<u32>().unwrap())
+                .collect::<Vec<u32>>()
+        })
+        .collect();
+
+    for i in 0..(counts.get(0).unwrap().len()) {
+        if counts.len() == 1 {
+            break;
+        }
+        let total = counts.iter().map(|v| v.get(i).unwrap().to_owned()).sum();
+        let to_keep = if comp(total, counts.len().try_into().unwrap()) {
+            1u32
+        } else {
+            0u32
+        };
+        counts.retain(|v| v.get(i).unwrap() == &to_keep);
+    }
+
+    return u32::from_str_radix(
+        &counts
+            .get(0)
+            .unwrap()
+            .into_iter()
+            .map(|v| v.to_string())
+            .collect::<Vec<String>>()
+            .join(""),
+        2,
+    )
+    .unwrap();
 }
 
 fn get_rates<F>(input: &Vec<&str>, comp: F) -> u32
@@ -61,7 +102,7 @@ const INPUT: &str = include_str!("input.txt");
 
 #[cfg(test)]
 mod tests {
-    use crate::day3::{get_input, get_rates};
+    use crate::day3::{get_input, get_rates, get_rating};
 
     const TEST_INPUT: &str = "00100
 11110
@@ -81,12 +122,12 @@ mod tests {
         let parsed_input = get_input(TEST_INPUT);
         assert_eq!(get_rates(&parsed_input, |a, b| a > b), 22);
         assert_eq!(get_rates(&parsed_input, |a, b| a < b), 9);
-        //assert_eq!(find_pos(parsed_input), 150);
     }
 
-    // #[test]
-    // fn part_2() {
-    //     let parsed_input = get_input(TEST_INPUT);
-    //     assert_eq!(find_pos2(parsed_input), 900);
-    // }
+    #[test]
+    fn part_2() {
+        let parsed_input = get_input(TEST_INPUT);
+        assert_eq!(get_rating(&parsed_input, |a, b| a * 2 >= b), 23);
+        assert_eq!(get_rating(&parsed_input, |a, b| a * 2 < b), 10);
+    }
 }
