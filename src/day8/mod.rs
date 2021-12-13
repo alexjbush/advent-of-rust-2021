cf87baeef16f2ea6843fd0687acadb9e5ca7c352
Alex Bush <alexbush@kpmg.co.nz>
2021-12-13 14:22:20 +1300

    Day 8

--- src/day8/mod.rs
+++ src/day8/mod.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::collections::HashSet;
 
 use itertools::Itertools;
 
@@ -20,9 +21,7 @@ fn task1() -> String {
 }
 
 fn task2() -> String {
-    // let input = parse_input(INPUT);
-
-    "N/A".to_string()
+    part2(INPUT).to_string()
 }
 
 fn part1(input: &str) -> usize {
@@ -42,35 +41,104 @@ fn part1(input: &str) -> usize {
         .count()
 }
 
-fn part2(input: &str) -> usize {
-    input.lines().map(|l| parse_line(l));
-    1
+fn part2(input: &str) -> u32 {
+    input.lines().map(|l| parse_line(l)).sum()
 }
 
-fn parse_line(input: &str) {
+fn parse_line(input: &str) -> u32 {
     let (def, nums) = input.split_once("|").unwrap();
-    let mut map: HashMap<usize, Vec<Vec<char>>> = HashMap::new();
+    let mut map: HashMap<usize, Vec<HashSet<char>>> = HashMap::new();
     def.split_ascii_whitespace().for_each(|d| {
         map.entry(d.len())
             .or_insert_with(|| vec![])
-            .push(d.to_string().chars().sorted().collect_vec())
+            .push(d.to_string().chars().sorted().collect())
     });
-    let mut res: HashMap<&Vec<char>, usize> = HashMap::new();
+    let mut res: HashMap<char, &HashSet<char>> = HashMap::new();
     //1
-    res.insert(map.get(&2).unwrap().get(0).unwrap(), 1);
+    res.insert('1', map.get(&2).unwrap().get(0).unwrap());
     //7
-    res.insert(map.get(&3).unwrap().get(0).unwrap(), 7);
+    res.insert('7', map.get(&3).unwrap().get(0).unwrap());
     //4
-    res.insert(map.get(&4).unwrap().get(0).unwrap(), 4);
+    res.insert('4', map.get(&4).unwrap().get(0).unwrap());
     //8
-    res.insert(map.get(&7).unwrap().get(0).unwrap(), 8);
+    res.insert('8', map.get(&7).unwrap().get(0).unwrap());
+    //6
+    res.insert(
+        '6',
+        map.get(&6)
+            .unwrap()
+            .iter()
+            .find(|v| v.union(res.get(&'1').unwrap()).into_iter().count() == 7)
+            .unwrap(),
+    );
+    //0
+    res.insert(
+        '0',
+        map.get(&6)
+            .unwrap()
+            .iter()
+            .filter(|s| *s != *res.get(&'6').unwrap())
+            .find(|v| v.union(res.get(&'4').unwrap()).into_iter().count() == 7)
+            .unwrap(),
+    );
+    //9
+    res.insert(
+        '9',
+        map.get(&6)
+            .unwrap()
+            .iter()
+            .find(|s| *s != *res.get(&'6').unwrap() && *s != *res.get(&'0').unwrap())
+            .unwrap(),
+    );
+    //5
+    res.insert(
+        '5',
+        map.get(&5)
+            .unwrap()
+            .iter()
+            .find(|v| {
+                v.union(res.get(&'1').unwrap())
+                    .map(|v| v.clone())
+                    .collect::<HashSet<char>>()
+                    == **res.get(&'9').unwrap()
+            })
+            .unwrap(),
+    );
+    //2
+    res.insert(
+        '2',
+        map.get(&5)
+            .unwrap()
+            .iter()
+            .find(|v| v.union(res.get(&'4').unwrap()).into_iter().count() == 7)
+            .unwrap(),
+    );
+    //3
+    res.insert(
+        '3',
+        map.get(&5)
+            .unwrap()
+            .iter()
+            .find(|s| *s != *res.get(&'2').unwrap() && *s != *res.get(&'5').unwrap())
+            .unwrap(),
+    );
+
+    //Result time
+    nums.split_ascii_whitespace()
+        .map(|n| {
+            let r: HashSet<char> = n.chars().into_iter().collect();
+            res.iter().find(|(_, v)| ***v == r).unwrap().0
+        })
+        .join("")
+        .parse::<u32>()
+        .unwrap()
 }
 
 const INPUT: &str = include_str!("input.txt");
 
 #[cfg(test)]
 mod tests {
-    use crate::day8::part1;
+    use crate::day8::{part1, part2};
 
     const TEST_INPUT: &str =
         "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
@@ -91,7 +159,6 @@ gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
 
     #[test]
     fn part_2() {
-
-        // assert_eq!(part1(TEST_INPUT), 26);
+        assert_eq!(part2(TEST_INPUT), 61229);
     }
 }
