b470af340065da38f5c13ca3ebd747418d4c7048
Alex Bush <bushnoh@gmail.com>
2021-12-14 07:58:17 +1300

    Part 2

--- src/day10/mod.rs
+++ src/day10/mod.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+
 use crate::Day;
 pub struct Day10 {}
 
@@ -16,7 +18,7 @@ fn task1() -> String {
 }
 
 fn task2() -> String {
-    "N/A".to_string()
+    step_2(INPUT).to_string()
 }
 
 #[derive(Debug, PartialEq)]
@@ -28,7 +30,7 @@ enum BracketType {
 }
 
 impl BracketType {
-    fn get_points(&self) -> u32 {
+    fn get_corrupt_points(&self) -> u32 {
         match self {
             BracketType::Round => 3,
             BracketType::Square => 57,
@@ -36,8 +38,17 @@ impl BracketType {
             BracketType::Angle => 25137,
         }
     }
+    fn get_incomplete_points(&self) -> u64 {
+        match self {
+            BracketType::Round => 1,
+            BracketType::Square => 2,
+            BracketType::Curly => 3,
+            BracketType::Angle => 4,
+        }
+    }
 }
 
+#[derive(Debug)]
 struct Bracket {
     bracket: BracketType,
     open: bool,
@@ -98,19 +109,48 @@ fn get_first_unbalanced_bracket(input: &str) -> Option<BracketType> {
     None
 }
 
+fn get_completion_score(input: &str) -> u64 {
+    let mut stack: Vec<Bracket> = Vec::new();
+    for b in input.chars().into_iter().map(|c| Bracket::parse(c)) {
+        if b.open {
+            stack.push(b);
+        } else {
+            let last = stack.pop();
+            if last.map_or_else(|| true, |l| l.bracket != b.bracket) {
+                panic!("Line is corrupt: {:?}", input);
+            }
+        }
+    }
+    stack.reverse();
+    stack
+        .iter()
+        .fold(0, |z, b| 5 * z + b.bracket.get_incomplete_points())
+}
+
 fn step_1(input: &str) -> u32 {
     input
         .lines()
         .filter_map(|l| get_first_unbalanced_bracket(l))
-        .map(|b| b.get_points())
+        .map(|b| b.get_corrupt_points())
         .sum()
+}
+
+fn step_2(input: &str) -> u64 {
+    let r: Vec<u64> = input
+        .lines()
+        .filter(|l| get_first_unbalanced_bracket(l).is_none())
+        .map(|b| get_completion_score(b))
+        .sorted()
+        .collect();
+
+    *r.get(r.len() / 2).unwrap()
 }
 
 const INPUT: &str = include_str!("input.txt");
 
 #[cfg(test)]
 mod tests {
-    use crate::day10::step_1;
+    use crate::day10::{step_1, step_2};
 
     const TEST_INPUT: &str = "[({(<(())[]>[[{[]{<()<>>
 [(()[<>])]({[<{<<[]>>(
@@ -130,6 +170,6 @@ mod tests {
 
     #[test]
     fn part_2() {
-        // assert_eq!(step2(TEST_INPUT), 1134);
+        assert_eq!(step_2(TEST_INPUT), 288957);
     }
 }
