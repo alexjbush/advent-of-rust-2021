d8d00d015c889e7e04ea8a6d4fa719df1f4d5ab3
Alex Bush <bushnoh@gmail.com>
2021-12-14 19:39:14 +1300

    Day 11

--- src/day11/mod.rs
+++ src/day11/mod.rs
@@ -38,38 +38,67 @@ impl CavernMap {
     }
 }
 
-fn interate(map: &mut CavernMap) -> u64 {
+fn iterate(map: &mut CavernMap) -> u64 {
     let mut count = 0;
-    for (x, y) in (0..map.width).flat_map(|x| (0..map.height).map(move |y| (x, y))) {
-        let mut to_visit: Vec<(i32, i32)> = vec![(x, y)];
+    let list: Vec<(i32, i32)> = (0..map.width)
+        .flat_map(|x| (0..map.height).map(move |y| (x, y)))
+        .collect();
+    for (x, y) in list.iter() {
+        let mut to_visit: Vec<(i32, i32)> = vec![(*x, *y)];
         while to_visit.len() > 0 {
             let (t_x, t_y) = to_visit.pop().unwrap();
 
             map.get_coord(t_x, t_y).iter_mut().for_each(|v| {
                 let flash = **v == 9;
                 **v += 1;
-                (t_x - 1..=t_x + 1)
-                    .flat_map(|n_x| (t_y - 1..=t_y + 1).map(move |n_y| (n_x, n_y)))
-                    .filter(|(n_x, n_y)| *n_x != t_x && *n_y != t_y)
-                    .for_each(|n| to_visit.push(n));
+                if flash {
+                    (t_x - 1..=t_x + 1)
+                        .flat_map(|n_x| (t_y - 1..=t_y + 1).map(move |n_y| (n_x, n_y)))
+                        .for_each(|n| to_visit.push(n));
+                }
             })
+        }
+    }
+    for (x, y) in list.iter() {
+        let this = map.get_coord(*x, *y).unwrap();
+        if *this > 9 {
+            *this = 0;
+            count += 1;
         }
     }
     count
 }
 
 fn task1() -> String {
-    "N/A".to_string()
+    step_1(INPUT, 100).to_string()
 }
 
 fn task2() -> String {
-    "N/A".to_string()
+    step_2(INPUT).to_string()
+}
+
+fn step_1(input: &str, steps: usize) -> u64 {
+    let mut map = CavernMap::create(input);
+    (0..steps).map(|_| iterate(&mut map)).sum()
+}
+
+fn step_2(input: &str) -> u64 {
+    let mut map = CavernMap::create(input);
+    let mut step = 0;
+    loop {
+        step += 1;
+        let flashes = iterate(&mut map);
+        if flashes == 100 {
+            return step;
+        }
+    }
 }
 
 const INPUT: &str = include_str!("input.txt");
 
 #[cfg(test)]
 mod tests {
+    use crate::day11::{step_1, step_2};
 
     const TEST_INPUT: &str = "5483143223
 2745854711
@@ -84,11 +113,14 @@ mod tests {
 
     #[test]
     fn part_1() {
-        // assert_eq!(step_1(TEST_INPUT), 26397);
+        assert_eq!(step_1(TEST_INPUT, 0), 0);
+        assert_eq!(step_1(TEST_INPUT, 1), 0);
+        assert_eq!(step_1(TEST_INPUT, 2), 35);
+        assert_eq!(step_1(TEST_INPUT, 100), 1656);
     }
 
     #[test]
     fn part_2() {
-        // assert_eq!(step_2(TEST_INPUT), 288957);
+        assert_eq!(step_2(TEST_INPUT), 195);
     }
 }
